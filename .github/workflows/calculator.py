094386ec441f1ad505c813a569e5e33d70a2f081
Cem Bağlum <39911281+cembglm@users.noreply.github.com>
2024-02-26 19:53:15 +0300

    Add files via upload

---
+++ .github/workflows/calculator.py
@@ -0,0 +1,20 @@
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero."
+    else:
+        return x / y
+
+print(add(2,2))
+print(subtract(2,2))
+print(multiply(2,2))
+print(divide(2,2))
